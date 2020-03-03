4a1b364124f5b7716f0ec7c7ea42b32415e6af44
Natsuki <nthr7286@gmail.com>
2020-03-03 15:41:44 +0900

    Implement babel-loader and html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
-import _ from 'lodash'
-import { nijou, NAME } from './utilities'
-import Lion from './utilities'
-import './styles.css'
-import './styles.scss'
-import neko from './2400b26f95d0a4326165639f8dafc685_s.jpg'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-console.log(Lion.say())
-
-console.log(nijou(3))
-console.log(NAME)
-
-const component = () => {
-  const div = document.createElement('div')
-  const array = ['Hello', 'webpack', '!']
-  div.innerHTML = _.join(array, ' ')
-  return div
-}
-
-document.body.appendChild(component())
-document.body.classList.add('bg')
-
-const image = new Image()
-image.src = neko
-document.body.appendChild(image)
+ReactDOM.render(
+  <div>Hello, React!</div>,
+  document.getElementById('root'),
+);
